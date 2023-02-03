5f76b4ed1fb0462d1db06feeb050a0031e6d5b85
Joao Queiroz <joaovsq@hotmail.com>
2023-02-03 15:35:50 -0300

    aula_33

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,6 @@ class PrincipalController extends Controller
 {
     //método ou action, será chamado pela rota
     public function principal(){
-        echo 'Olá, Seja bem vindo ao curso!';
+       return view('site.principal');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,22 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria_id}', //
+function(
+  string $nome = 'Desconhecido',
+  int $categoria_id = 1 // 1 - 'informação'
+) 
+  {
+   echo "Estamos aqui: $nome - $categoria_id" ;      
+  }
+)->where('categoria_id', '[0-9]+')->where('nome','[A-Za-z]+');
+
 /* 
 Métodos http
 get
@@ -16,14 +32,21 @@ options
 */
 
 //no curso, ensina a fazer de uma maneira diferente, porém, que não funciona
-Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-
-Route::get('/contato', [ContatoController::class, 'contato']);
-
-
-
+//contato, terá os seguintes parâmetros:nome, categoria,assunto,mensagem
+//Para um parâmetro ser opcional, basta colocar ? na frente dele
+//feito isso, inicie o valor do parâmetro. para ele não vir vazio
+//------------------------------------------------------------------
+// Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', function(
+//    string $nome = "Desconhecido", 
+//    string $categoria = "Categoria não informada!", 
+//    string $assunto = "Assunto não informado!", 
+//    string $mensagem = "Mensagem não informada!"){
+//      echo "Colaborador $nome - $categoria <br>
+//      Assunto: $assunto <br>
+//      Mensagem: $mensagem ";
+//   }
+// );
 //rota criada de forma direta, sem passar por uma controller
 // Route::get('/contato', function () {
 //     return 'contato';
