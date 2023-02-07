bc82a9fc3ac1ef01e63d03f1c02ba45cdec03273
Joao Queiroz <joaovsq@hotmail.com>
2023-02-07 08:16:24 -0300

    commit_aula46_blade_if_else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,11 +8,26 @@
 {{' Olá Mundo! '}}
 
 <?php
+
   //Comentários de uma linha
   /*
      Comentários de múltiplas linhas 
   */
+{{-- if(){
+}else if(){
 
-  echo 'Texto de teste';
+}else {
+
+} --}}
 
 ?>
+{{-- dd inspenciona uma váriavel e nos mostra um print do que ela contém em tela--}}
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10 )
+<h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+<h3>Existem vários fornecedores cadastrados</h3>
+@else 
+<h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
